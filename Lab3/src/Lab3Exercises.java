9b299fbcbca0eb700fd6c2e268c0f13de3fc6f6a
4015b14 <4015b14@aguacate.ece.uprm.edu>
2017-11-15 18:31:59 -0400

    new features, color and more

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -3,9 +3,13 @@ import javax.swing.JFrame;
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Luis Romero");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		myFrame.setLocation(850, 400);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
+		
+		//adding my panel
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 	}
 }
